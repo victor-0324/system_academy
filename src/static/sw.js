9abdbcbad0e0892144410efc6e4ca157bb70e0ff
vitorlima0324 <xicovector@gmail.com>
2024-07-25 06:42:28 -0300

    update sw.js

--- src/static/sw.js
+++ src/static/sw.js
@@ -1,9 +1,4 @@
-const CACHE_NAME = "pwabuilder-offline";
-const PRECACHE_URLS = [
-  '/', // Add more URLs of your critical assets
-  '/static/css/style.css',
-  '/static/js/main.js'
-];
+const CACHE = "pwabuilder-offline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
@@ -13,31 +8,25 @@ self.addEventListener("message", (event) => {
   }
 });
 
-// Precache critical assets
-workbox.precaching.precacheAndRoute(PRECACHE_URLS);
-
-// Cache First strategy for CSS and JS files with background update
+// Use NetworkFirst for login and logout routes
 workbox.routing.registerRoute(
-  /\.(?:css|js)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: `${CACHE_NAME}-assets`,
+  new RegExp('/(login|logout|profile|dashboard)'),
+  new workbox.strategies.NetworkFirst({
+    cacheName: CACHE,
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 50,
         maxAgeSeconds: 7 * 24 * 60 * 60, // 1 week
       }),
-      new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
     ],
   })
 );
 
-// Network First strategy for HTML pages
+// Use StaleWhileRevalidate for all other routes
 workbox.routing.registerRoute(
   new RegExp('/.*'),
-  new workbox.strategies.NetworkFirst({
-    cacheName: `${CACHE_NAME}-pages`,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE,
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 50,
@@ -46,25 +35,3 @@ workbox.routing.registerRoute(
     ],
   })
 );
-
-// Preload important resources
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(PRECACHE_URLS);
-    })
-  );
-});
-
-// Prefetch next likely navigation pages
-self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.waitUntil(
-      fetch(event.request).then(response => {
-        return caches.open(CACHE_NAME).then(cache => {
-          return cache.put(event.request, response.clone());
-        });
-      })
-    );
-  }
-});
