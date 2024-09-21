379257933d626677a815614f8266c3940a62d19f
vitorlima0324 <xicovector@gmail.com>
2024-09-20 21:09:07 -0300

    update

--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,7 @@ class Config:
     load_dotenv()
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv("MYSQL_PRIVATE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 class DevelopmentConfig(Config):
     DEBUG = True
